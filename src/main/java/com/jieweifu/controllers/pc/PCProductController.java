dd8fc4da7e8aa7938647dab1a4b9c9b00df9717d
surick <jk103@qq.com>
2018-12-12 16:16:55 +0800

    add cn/en change feature

--- src/main/java/com/jieweifu/controllers/pc/PCProductController.java
+++ src/main/java/com/jieweifu/controllers/pc/PCProductController.java
@@ -1,7 +1,13 @@
 package com.jieweifu.controllers.pc;
 
+import com.jieweifu.models.cms.Products;
+import com.jieweifu.services.cms.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Jin
@@ -10,13 +16,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PCProductController {
 
+    @Autowired
+    ProductsService productsService;
+
     @GetMapping("products_list")
-    public String listProducts() {
+    public String listProducts(Map<String, Object> map) {
+        List<Products> list = productsService.getAllProducts();
+        map.put("list", list);
         return "products_list";
     }
 
     @GetMapping("/cn/products_list")
-    public String cnListProducts() {
+    public String cnListProducts(Map<String, Object> map) {
+        List<Products> list = productsService.getAllProducts();
+        map.put("list", list);
         return "cn/products_list";
     }
 }
